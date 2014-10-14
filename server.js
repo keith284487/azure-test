a3833e1ad21b09fcc069c1ed28e57ed8e02d737d
Kirill Nikiforov <nikiforov.kirill@live.com>
2014-10-14 13:26:55 +0400

    express

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var http = require("http");
- 
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('FYI');
+});
+
 http.createServer(function (req, res) {
 res.writeHead(200, {"Content-Type": "text/plain"});
 res.end("Hello Node.js and Windows Azure Website!\n");
