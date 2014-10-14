873bfe156b47201b2e666a0b85dadb3ebc7ba00d
Kirill Nikiforov <nikiforov.kirill@live.com>
2014-10-14 13:29:16 +0400

    we

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('FYI');
+    res.send('Hello you there');
 });
 
 http.createServer(function (req, res) {
